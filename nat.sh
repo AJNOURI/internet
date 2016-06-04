c9c95d548b211f3fba0880dba3d416896b8b59af
AJ NOURI <ajn@localhost.localdomain>
2016-06-04 07:08:51 +0200

    add nat script

---
+++ nat.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+
+LAN="eth1"
+INT="eth0"
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i $LAN -j ACCEPT
+iptables --t nat -F POSTROUTING
+iptables --t nat -A POSTROUTING -o $INT -j MASQUERADE
