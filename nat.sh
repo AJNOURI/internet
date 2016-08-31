44ce0654306d8f1835af19ee4a9f62eb5b724f66
AJ NOURI <ajn.bin@gmail.com>
2016-08-31 05:08:52 +0200

    Update nat.sh

--- nat.sh
+++ nat.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-mkdir -p /etc/dnsmasq
+if [ ! -f /etc/dnsmasq/dnsmaq.conf ]; then
+  mkdir -p /etc/dnsmasq
+  cat > /etc/dnsmasq/dnsmaq.conf <<EOF
+  # dnsmasq will open tcp/udp port 53 and udp port 67 to world to help with
+  # dynamic interfaces (assigning dynamic ips). Dnsmasq will discard world
+  # requests to them, but the paranoid might like to close them and let the 
+  # kernel handle them:
+  bind-interfaces
+  # Dynamic range of IPs to make available to LAN pc
+  
+  # First LAN interface
+  interface=eth2
+  dhcp-option=eth2,3,10.0.0.1
+  dhcp-option=eth2,6,192.168.0.254
+  dhcp-range=eth2,10.0.0.10,10.0.0.20,12h
+  
+  # Second LAN interface
+  interface=eth0
+  dhcp-option=eth0,3,11.0.0.1
+  dhcp-option=eth0,6,192.168.0.254
+  dhcp-range=eth0,11.0.0.10,11.0.0.20,12h
 
-cat > /etc/dnsmasq/dnsmaq.conf <<EOF
-# dnsmasq will open tcp/udp port 53 and udp port 67 to world to help with
-# dynamic interfaces (assigning dynamic ips). Dnsmasq will discard world
-# requests to them, but the paranoid might like to close them and let the 
-# kernel handle them:
-bind-interfaces
-# Dynamic range of IPs to make available to LAN pc
-
-# First LAN interface
-interface=eth2
-dhcp-option=eth2,3,10.0.0.1
-dhcp-option=eth2,6,192.168.0.254
-dhcp-range=eth2,10.0.0.10,10.0.0.20,12h
-
-# Second LAN interface
-interface=eth0
-dhcp-option=eth0,3,11.0.0.1
-dhcp-option=eth0,6,192.168.0.254
-dhcp-range=eth0,11.0.0.10,11.0.0.20,12h
-
-EOF
+  EOF
+fi
 
 # NAT (masquerade) deployment
 iptables -F
