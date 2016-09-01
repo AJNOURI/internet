08c5994e470b6cf8fc89c679171e727bc57fe23c
AJ NOURI <ajn@localhost.localdomain>
2016-09-01 06:26:35 +0200

    Keep prior config + ask if run dhcp

--- nat.sh
+++ nat.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+dhcp(){                                                           
+        echo "Edit /etc/dnsmasq/dnsmaq.conf to change the configuration"   
+        dnsmasq --log-dhcp --conf-file=/etc/dnsmasq/dnsmaq.conf            
+}
+
 if [ ! -f /etc/dnsmasq/dnsmaq.conf ]; then
   mkdir -p /etc/dnsmasq
   cat > /etc/dnsmasq/dnsmaq.conf <<EOF
@@ -11,18 +16,18 @@ if [ ! -f /etc/dnsmasq/dnsmaq.conf ]; then
   # Dynamic range of IPs to make available to LAN pc
   
   # First LAN interface
-  interface=eth2
-  dhcp-option=eth2,3,10.0.0.1
-  dhcp-option=eth2,6,192.168.0.254
-  dhcp-range=eth2,10.0.0.10,10.0.0.20,12h
+  interface=ethi1
+  dhcp-option=eth1,3,192.168.114.100
+  dhcp-option=eth1,6,8.8.8.8
+  dhcp-range=eth1,192.168.114.10,192.168.114.20,12h
   
   # Second LAN interface
-  interface=eth0
-  dhcp-option=eth0,3,11.0.0.1
-  dhcp-option=eth0,6,192.168.0.254
-  dhcp-range=eth0,11.0.0.10,11.0.0.20,12h
+  #interface=eth0
+  #dhcp-option=eth0,3,11.0.0.1
+  #dhcp-option=eth0,6,192.168.0.254
+  #dhcp-range=eth0,11.0.0.10,11.0.0.20,12h
+EOF
 
-  EOF
 fi
 
 # NAT (masquerade) deployment
@@ -37,15 +42,19 @@ iptables -P FORWARD ACCEPT
 iptables -P OUTPUT ACCEPT
 
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -i eth0 -j ACCEPT
-iptables -A FORWARD -i eth2 -j ACCEPT
+#iptables -A FORWARD -i eth0 -j ACCEPT
+iptables -A FORWARD -i eth1 -j ACCEPT
 iptables --t nat -F POSTROUTING
-iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
-iptables -A FORWARD -i eth2 -o eth1 -j ACCEPT
-iptables --t nat -A POSTROUTING -o eth1 -j MASQUERADE
+iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+#iptables -A FORWARD -i eth1 -o eth1 -j ACCEPT
+iptables --t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-echo "Edit /etc/dnsmasq/dnsmaq.conf to change the configuration"
-dnsmasq --log-dhcp --no-daemon --conf-file=/etc/dnsmasq/dnsmaq.conf 
-
-
+read -p 'Would you like to run the dhcp server (dnsmasq)?  [Yy] [Nn]  ' ISDHCP
+while true; do
+  case $ISDHCP in
+    [Nn]* ) break 1;;
+    [Yy]* ) dhcp;;
+    * ) echo "Please answer yes [Yy]* or no [Nn] *";;
+   esac
+done
 
